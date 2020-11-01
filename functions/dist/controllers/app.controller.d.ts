64f2b0b265b28fed43386159d5d4edaa9c54207e
=okakyo <=0622okakyo@gmail.com>
2020-11-02 04:08:16 +0900

    Firebase にNestJSを実装

--- functions/dist/controllers/app.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { AppService } from "@/services/app.service";
-export declare class AppController {
-    private appsService;
-    constructor(appsService: AppService);
-    hello(): string;
-}
