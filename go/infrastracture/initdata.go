46c1a6d9bd0101e4f56ff874cc3a111ce1619c00
Kume Yuki <khmer.program.495@gmail.com>
2020-11-01 22:40:26 +0900

    ADD create environment

---
+++ go/infrastracture/initdata.go
@@ -0,0 +1,12 @@
+package infrastracture
+
+import "github.com/jphacks/F_2002_1/go/entity/model"
+
+var Examples = model.Examples{
+	model.Example{
+		Name: "example1",
+	},
+	model.Example{
+		Name: "example2",
+	},
+}
