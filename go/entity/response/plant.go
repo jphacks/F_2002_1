89ead042394ca2802dc10c906942f25279eca771
Kume Yuki <khmer.program.495@gmail.com>
2020-11-03 19:36:43 +0900

    ADD plantのreq, resのstruct

---
+++ go/entity/response/plant.go
@@ -0,0 +1,41 @@
+package response
+
+import "time"
+
+type Season struct {
+	From int `json:"from"`
+	To   int `json:"to"`
+}
+
+type Temperature struct {
+	Id    int     `json:"id"`
+	Name  string  `json:"name"`
+	Value float32 `json:"value"`
+}
+
+type Water struct {
+	Id    int     `json:"id"`
+	Name  string  `json:"name"`
+	Value float32 `json:"value"`
+}
+
+type Plant struct {
+	ID           int           `json:"id"`
+	CreatedAt    time.Time     `json:"created_at"`
+	UpdatedAt    time.Time     `json:"updated_at"`
+	DeletedAt    *time.Time    `json:"deleted_at"`
+	Name         string        `json:"name"`
+	NickName     string        `json:"nick_name"`
+	Price        string        `json:"price"`
+	Period       int           `json:"period"`
+	Difficulty   int           `json:"difficulty"`
+	Description  string        `json:"description"`
+	KitName      string        `json:"kit_name"`
+	Season       Season        `json:"season"`
+	Temperatures []Temperature `json:"temperatures"`
+	Waters       []Water       `json:"waters"`
+}
+
+type PlantGet []Plant
+
+type PlantGetById Plant
