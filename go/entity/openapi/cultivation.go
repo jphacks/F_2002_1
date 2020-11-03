ea20903c7f82b2d49b3ce9b1b9ec53ad61fb1a80
Kume Yuki <khmer.program.495@gmail.com>
2020-11-03 23:29:09 +0900

    ADD usersのreq, resのstruct

---
+++ go/entity/openapi/cultivation.go
@@ -0,0 +1,20 @@
+package openapi
+
+import "time"
+
+type record struct {
+	Waterings   []Watering   `json:"watering"`
+	Harvestings []Harvesting `json:"harvesting"`
+}
+
+type Cultivation struct {
+	ID                  int        `json:"id"`
+	CreatedAt           time.Time  `json:"created_at"`
+	UpdatedAt           time.Time  `json:"updated_at"`
+	DeletedAt           *time.Time `json:"deleted_at"`
+	Plant               Plant      `json:"plant"`
+	NickName            string     `json:"nick_name"`
+	StartCultivatingAt  time.Time  `json:"start_cultivating_at"`
+	FinishCultivatingAt time.Time  `json:"finish_cultivating_at"`
+	Record              record     `json:"record"`
+}
---
+++ go/entity/openapi/plant.go
@@ -0,0 +1,20 @@
+package openapi
+
+import "time"
+
+type Plant struct {
+	ID           int           `json:"id"`
+	CreatedAt    time.Time     `json:"created_at"`
+	UpdatedAt    time.Time     `json:"updated_at"`
+	DeletedAt    *time.Time    `json:"deleted_at"`
+	Name         string        `json:"name"`
+	NickName     string        `json:"nick_name"`
+	Price        string        `json:"price"`
+	Period       int           `json:"period"`
+	Difficulty   int           `json:"difficulty"`
+	Description  string        `json:"description"`
+	KitName      string        `json:"kit_name"`
+	Season       Season        `json:"season"`
+	Temperatures []Temperature `json:"temperatures"`
+	Waters       []Water       `json:"waters"`
+}
