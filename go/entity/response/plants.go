d9a20c79f44a5c454e647f9b1a9cbcc9c2bde3a9
Kume Yuki <khmer.program.495@gmail.com>
2020-11-03 20:34:06 +0900

    FIX openapiのディレクトリを追加

--- go/entity/response/plants.go
+++ go/entity/response/plants.go
@@ -1,47 +1,13 @@
 package response
 
-import "time"
+import "github.com/jphacks/F_2002_1/go/entity/openapi"
 
-type Season struct {
-	From int `json:"from"`
-	To   int `json:"to"`
-}
+type PlantGet []openapi.Plant
 
-type Temperature struct {
-	Id    int     `json:"id"`
-	Name  string  `json:"name"`
-	Value float32 `json:"value"`
-}
+type PlantGetById openapi.Plant
 
-type Water struct {
-	Id    int     `json:"id"`
-	Name  string  `json:"name"`
-	Value float32 `json:"value"`
-}
+type PlantPost openapi.Plant
 
-type Plant struct {
-	ID           int           `json:"id"`
-	CreatedAt    time.Time     `json:"created_at"`
-	UpdatedAt    time.Time     `json:"updated_at"`
-	DeletedAt    *time.Time    `json:"deleted_at"`
-	Name         string        `json:"name"`
-	NickName     string        `json:"nick_name"`
-	Price        string        `json:"price"`
-	Period       int           `json:"period"`
-	Difficulty   int           `json:"difficulty"`
-	Description  string        `json:"description"`
-	KitName      string        `json:"kit_name"`
-	Season       Season        `json:"season"`
-	Temperatures []Temperature `json:"temperatures"`
-	Waters       []Water       `json:"waters"`
-}
-
-type PlantGet []Plant
-
-type PlantGetById Plant
-
-type PlantPost Plant
-
-type PlantPutById Plant
+type PlantPutById openapi.Plant
 
 type PlantDeleteById struct{}
