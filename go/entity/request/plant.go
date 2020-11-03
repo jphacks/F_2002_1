031daeef530924d652795c913db47183a9c0f870
Kume Yuki <khmer.program.495@gmail.com>
2020-11-03 19:40:03 +0900

    ADD openapiでgeneralタグついてないやつも一応追加

--- go/entity/request/plant.go
+++ go/entity/request/plant.go
@@ -5,3 +5,16 @@ type PlantGet struct{}
 type PlantGetById struct {
 	PlantId int `json:"plant_id"`
 }
+
+type PlantPost struct {
+	Name string `json:"name"`
+}
+
+type PlantPutById struct {
+	PlantId int    `json:"plant_id"`
+	Name    string `json:"name"`
+}
+
+type PlantDeleteById struct {
+	PlantId int `json:"plant_id"`
+}
