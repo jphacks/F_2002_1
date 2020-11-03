1278564ad99222c45a49d9438d7e81633a287f63
Kume Yuki <khmer.program.495@gmail.com>
2020-11-03 19:09:28 +0900

    ADD apiのreqとresのstructを定義するディレクトリと例を追加

---
+++ go/entity/response/example.go
@@ -0,0 +1,21 @@
+package response
+
+import "time"
+
+type Example struct {
+	ID        int        `gorm:"primary_key"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+	Name      string     `json:"name"`
+}
+
+type ExampleGet []Example
+
+type ExampleGetById Example
+
+type ExamplePost Example
+
+type ExamplePutById Example
+
+type ExampleDeleteById struct{}
