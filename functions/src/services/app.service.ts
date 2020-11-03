95cc9e6186c8e2f01c3813e1ae209ba14794f529
=okakyo <=0622okakyo@gmail.com>
2020-11-03 22:03:22 +0900

    環境構築中

--- functions/src/services/app.service.ts
+++ functions/src/services/app.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import {database} from "../plugins/firebase.config";
+
+
+type Chat = {
+  id?:string,
+  from: string,
+  message: string,
+  createdAt:string,
+}
 
 @Injectable()
 export class AppService {
@@ -6,5 +15,18 @@ export class AppService {
     return "Hello World!";
   }
 
-  // 
+  /**
+   * テキストメッセージをサーバーに送信する
+   * @param roomId :string, 送信先のチャットIDを送信する
+   * @param message:string, 
+  */
+  public sendText(roomId:string, chat:Chat){
+
+    // database.ref(); // ここで Firestore へデータを送信する
+    return;
+  }
+  
+  public 
+
+
 }
