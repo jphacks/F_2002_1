f5596475b0b893f8f26cd892a34c5ffabf389073
=okakyo <=0622okakyo@gmail.com>
2020-11-04 16:01:48 +0900

    動作確認

--- functions/src/plugins/firebase.config.ts
+++ functions/src/plugins/firebase.config.ts
@@ -1,8 +1,19 @@
 import * as firebase from 'firebase-admin'
 
+/*
+const firebasePlugin:firebase.ServiceAccount = {
+  projectId: process.env.project_id,
+  privateKey: process.env.private_key,
+  clientEmail: process.env.client_email,
+}
+*/
+
 firebase.initializeApp({
   credential: firebase.credential.applicationDefault(),
+  databaseURL: "https://speak-vegetable.firebaseio.com"
 })
+
+
 
 export const database = firebase.database();
 export const firestore = firebase.firestore();
