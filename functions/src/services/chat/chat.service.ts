e6d7a9ed0ed1f2b0cd263b107fec191e5159ea08
=okakyo <=0622okakyo@gmail.com>
2020-11-07 01:58:07 +0900

    現在の進捗をpush

--- functions/src/services/chat/chat.service.ts
+++ functions/src/services/chat/chat.service.ts
@@ -4,10 +4,20 @@ import * as firebase from "firebase-admin";
 @Injectable()
 export class ChatService {
     public async getChatMessage(roomId:string){
-        
         try {
             const chatRoom= firebase.database().ref("chat_room").child(roomId)
-            const getData = await chatRoom.on("value",snapshot=>snapshot.val());
+            let getData: any = await chatRoom.once("value")
+            if (getData) {
+                const rootList = getData.val();
+                const  list = [];
+                // データオブジェクトを配列に変更する
+                if(rootList !== null) {
+                    Object.keys(rootList).forEach((val) => {
+                        list.push(rootList[val]);
+                    })
+                }
+                getData = list;
+            }
             return getData;
         } catch(e){
             console.error(e);
@@ -16,7 +26,7 @@ export class ChatService {
     }
     public async sendChatMessage(roomId:string,userId:string,message:string){
         try {
-            const chatRoom = firebase.database().ref("chat_room").child("roomId");
+            const chatRoom = firebase.database().ref("chat_room").child(roomId);
             const chatKey = await chatRoom.push().key
             const sendMessage = await chatRoom.child(chatKey).set({
                 id:chatKey,
