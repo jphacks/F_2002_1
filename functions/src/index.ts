fcec093f64181e542b26c970bf2f45dc0d271c4e
=okakyo <=0622okakyo@gmail.com>
2020-11-26 11:57:55 +0900

    Functions 周りを実装(今回のハッカソンでは結局不要でした)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,28 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
-
 admin.initializeApp()
 
 // Send Message to the server
-exports.postChat = functions.database.ref("") .onCreate((snap, context) => {
-  const appOptions = JSON.parse(process.env.FIREBASE_CONFIG);
-  appOptions.databaseAuthVariableOverride = context.auth;
-  const app = admin.initializeApp(appOptions, 'app');
-  const uppercase = snap.val();
+exports.post = functions.database.ref("/device_data/{deviceId}/{phone_udid}").onWrite(async (snap, context) => {
+  const deviceId = context.params.deviceId;
+  const registedData = snap.after.val();
+  const registedHumidityData = registedData.humidity;
+  const registedTemperatureData = registedData.temperature;
+  const registedPressureData =registedData.pressure;
+  const registedSolidMoistureData = registedData.solid_moisture;
+  const registedIlluminanceData = registedData.illuminance;
+  if(
+    registedHumidityData.status!=="ok"||
+    registedTemperatureData.status!=="ok" ||
+    registedPressureData.status!=="ok" ||
+    registedSolidMoistureData.status!=="ok" ||
+    registedIlluminanceData.status!=="ok"
+  ){
+      // TODO: ここに通知機能や外部APIなどの連携を実装する
+      functions.logger.info("通知を送ります");
+  }
+  
   
 });
+
